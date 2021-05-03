709efa2f7c000e77e03a1e948dcbeda6b7921a6b
Darwish Mat Zain <62579706+darwishzain@users.noreply.github.com>
2021-05-03 17:20:25 +0800

    update gui
    made some ui on app,java. for tomorrow's presentation.
    commented some of code in order.java cause it have some errors

--- src/App.java
+++ src/App.java
@@ -1,4 +1,26 @@
+
+
+import javax.swing.*;
+import java.awt.*;
+
 public class App {
+    static void registerUI(){
+        JFrame frame = new JFrame("FKIS");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(960,540);
+
+        JPanel loginPanel = new JPanel();
+        JTextField userId = new JTextField("ID",20);
+        JTextField userPwd = new JTextField("Password",20);
+        JButton loginBtn = new JButton("Log In");
+        loginPanel.add(userId);
+        loginPanel.add(userPwd);
+        loginPanel.add(loginBtn);
+       
+        frame.getContentPane().add(loginPanel);
+        frame.setVisible(true);
+
+    }
     public static void main(String[] args) throws Exception {
         request b = new request();
         order o = new order();
@@ -6,5 +28,7 @@ public class App {
         approve a = new approve();//test comment
         audit d = new audit();
         register u = new register();
+
+        registerUI();
     }
 }
