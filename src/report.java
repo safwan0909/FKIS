f95ef1c49b77dfbb85e85d2c28935d08dcbecd24
safwan0909 <83591914+safwan0909@users.noreply.github.com>
2021-05-03 22:22:17 +0800

    Update report.java

--- src/report.java
+++ src/report.java
@@ -1,3 +1,29 @@
-public class report {
+import java.util.Scanner;
+
+public class report 
+{
+  Scanner sc=new Scanner(System.in);
+  public void WriteReport()
+  {
+  System.out.print("WRITE REPORT INTERFACE/n");
+  System.out.print("Enter Report ID:");
+  String Report_ID=sc.next();
+  System.out.print("Enter Report Date:");
+  String Report_Date=sc.next();
+  System.out.print("Enter Approved Booking:");
+  String Approved_Booking=sc.next();
+  System.out.print("Enter Audit ID:");
+  String Audit_ID=sc.next();
+  System.out.print("Enter Order ID:");
+  String Order_ID=sc.next();
+  }
     
+  public void DisplayFinalReport()
+  {
+    System.out.println("Report ID:"+Report_ID);
+    System.out.println("Report Date:"+Report_Date);
+    System.out.println("Approved Booking:"+Approved_Booking);
+    System.out.println("Audit ID:"+Audit_Date);
+    System.out.println("Order ID:"+Order ID);
+  }
 }
