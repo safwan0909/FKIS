095944ec0b5d5811a8b737e8ad6771324bbd950b
Darwish Mat Zain <62579706+darwishzain@users.noreply.github.com>
2021-05-03 11:06:50 +0800

    luqman updated code

--- src/order.java
+++ src/order.java
@@ -1,3 +1,28 @@
-public class order {
+import java.util.Scanner;
+
+public class order extends register{
     
+void InformOrder {
+Scanner sc = new Scanner(System.in);
+int choice = 0;
+while(choice == 0)
+{    
+    System.out.println("Enter item code:");
+    String code = sc.nextLine();
+        
+    System.out.println("Enter item quantity:");
+    int quantity = sc.nextInt();
+        
+    System.out.println("Enter 0 to continue order or 1 to exit");
+    choice = sc.nextInt();    
+}
+
+    InformOrder list = new InformOrder();
+    String ID = list.UserRegister();
+    
+    System.out.println("UserID: "+ID);
+    System.out.println("Item Code: "+code);
+    System.out.println("Item Quantity: "+quantity);
+    }
+
 }
